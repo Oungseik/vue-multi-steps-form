2968763f64738a37a335775de37558855f1957b2
Oungseik <mhemaungthuwin@gmail.com>
2023-03-25 08:11:12 +0630

    style: add style to the last step on the thank you step

--- src/components/QuestionSteps.vue
+++ src/components/QuestionSteps.vue
@@ -3,7 +3,10 @@
     <li
       class="border-2 border-light-gray font-bold text-2xl rounded-full w-10 h-10 flex items-center justify-center text-light-gray"
       v-for="step in [...Array(steps).keys()]"
-      :class="{ 'text-marine-blue bg-pastel-blue border-pastel-blue': currentStep === step }"
+      :class="{
+        'text-marine-blue bg-pastel-blue border-pastel-blue':
+          currentStep === step || (step === steps - 1 && currentStep === steps)
+      }"
       :key="step"
     >
       {{ step + 1 }}
